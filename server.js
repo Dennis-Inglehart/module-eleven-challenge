d788cb4bb5ab19ea962dc5902ed6102f56f43387
Dennis-Inglehart <Haikon@twosixfour.org>
2023-06-08 12:58:30 -0400

    add delete route and individual get route

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Begin shit ChatGPT recommended:
-
 app.use(express.json());
 const dbFilePath = path.join(__dirname, 'db', 'db.json');
 const readDataFromFile = () => {const data = fs.readFileSync(dbFilePath, 'utf8');return JSON.parse(data);};
 const writeDataToFile = (data) => {fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2));};
 
+// assigns an integer value to the "id" key of a new note; called by app.post('/api/notes')
 const generateId = () => {
     const notes = readDataFromFile();
-    let maxId = 0;
-    for (const note of notes) {if (note.id > maxId) { maxId = note.id; }}  
-    return maxId + 1;
+    let biggestIdYet = 0;
+    for (const note of notes) {if (note.id > biggestIdYet) { biggestIdYet = note.id; }}  
+    return biggestIdYet + 1;
   };
 
 // UI(?) route to the front page. Pretty sure this never gets used....
@@ -35,7 +34,16 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-// API route to an individual note (coming soon!)
+// API route to an individual note
+app.get('/api/notes/:id', (req, res) => {
+  const notes = readDataFromFile();
+  const noteId = Number(req.params.id);
+  const note = notes.find((note) => note.id === noteId);
+
+  if (note) {
+    res.json(note);
+  } else {res.status(404).json({ error: 'Note not found' });}
+});
 
 // route to save a new note; works currently returns naked JSON data of the note, which is janky, and it ought to be a pretty, UI-looking display
 app.post('/api/notes', (req, res) => {
@@ -47,18 +55,14 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
   });
 
-// WIP route to delete a note; currently doesn't work because db.json does not have an `id` kvp!
+// route to delete a note
 app.delete('/api/notes/:id', (req, res) => {
     const notes = readDataFromFile();
-    const noteId = req.params.id;
+    const noteId = Number(req.params.id);
     const updatedNotes = notes.filter((note) => note.id !== noteId);
     writeDataToFile(updatedNotes);
-    res.sendStatus(204); // No Content
+    res.sendStatus(204);
 });
 
-// Serve static files from the public directory
 app.use(express.static('public'));
-
-// End shit ChatGPT recommended
-
 app.listen(PORT, () => {console.log(`Now listening at port ${PORT}`);});
